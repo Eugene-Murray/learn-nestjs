8e4226bd23440abf272efbde37ac6c91b48c482c
Eugene Murray <eugene.murray@gmail.com>
2024-10-21 21:38:43 +0100

    Add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from './services/app.service';
+import { NosqlDataModule } from './modules/nosql-data/nosql-data.module';
+import { SqlDataModule } from './modules/sql-data/sql-data.module';
+import { SecurityModule } from './modules/security/security.module';
+import { FilesModule } from './modules/files/files.module';
 
 @Module({
-  imports: [],
+  imports: [NosqlDataModule, SqlDataModule, SecurityModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
